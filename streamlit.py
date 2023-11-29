bd1a015a7e75e20ba3ed08864d7b824d0bd3bf5d
NgoQue <110570104+NgoQue@users.noreply.github.com>
2023-11-30 04:25:58 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -19,6 +19,11 @@ In the materials specifications:
 \n\n - d represents the inner radius of the cylinder
 \n\n - D represents the outer radius of the cylinder
 \n\n - h1 represents the center-to-center distance between two adjacent cylinders with a default value of 430nm""")
+#--------------------------------set up enviroment-------#
+ # Chạy lệnh tạo máy chủ ảo Xvfb
+os.system("Xvfb :1 -screen 0 1280x1024x24 -auth localhost")
+# Thiết lập biến môi trường DISPLAY
+os.environ["DISPLAY"] = ":1"
 #-----------------------------Lap ham cac thuat toan ML--------------------------------#
 def Au_model(d):
     global abs_wl
@@ -166,10 +171,10 @@ if st.button("Run"):
                 y3 = 430 + D/2 + 100 + (r)*np.sin(theta)
                 mlab.mesh(x3, y3, h, color=(0.8, 0.7, 0.65))
     #--------------------------------------------------------------------#
-    # Chạy lệnh tạo máy chủ ảo Xvfb
-    os.system("Xvfb :1 -screen 0 1280x1024x24 -auth localhost")
-    # Thiết lập biến môi trường DISPLAY
-    os.environ["DISPLAY"] = ":1"
+    # # Chạy lệnh tạo máy chủ ảo Xvfb
+    # os.system("Xvfb :1 -screen 0 1280x1024x24 -auth localhost")
+    # # Thiết lập biến môi trường DISPLAY
+    # os.environ["DISPLAY"] = ":1"
     with col1: 
         st.subheader("Image of the material")
         img3d(number)
