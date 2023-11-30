8b85158f3e64c847701678ba0555594078f0fa61
NgoQue <110570104+NgoQue@users.noreply.github.com>
2023-11-30 08:18:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import joblib
-# from mayavi import mlab
+from mayavi import mlab
 from pandas import DataFrame 
 from PIL import Image
 import os
@@ -157,27 +157,14 @@ if st.button("Run Spectral Absorption Prediction"):
         ax.set_ylim([0, 1])
         st.pyplot(fig)
     #----------------Hieu suat hap thu-------------------------------------------------#
-    # H = 0
-    # for i in range(0, 1002, 1):
-    #     H += ((abs_wl[i+1, 1]+abs_wl[i, 1])/2) *(abs_wl[i+1, 0] - abs_wl[i, 0] )
-    # H = H/2700
     st.write("The average absorption efficiency of :blue[**%s**] at d =  **%d** nm, h1 = **%d** nm is: **%.2f** %% " %(add_selectbox, number, number+30, 100*efficiency(abs_wl)))
     st.write('The maximum absorption efficiency of :blue[**%s**] is **%.2f** %% at d= **%d** nm , h1 = **%d** nm' %(add_selectbox, H0*100, D0, D0+30))
-
     
     #------------------------------Tao va lưu du lieu------------------------------------#
     with col2:
         # st.write("Data")
         data = pd.DataFrame(abs_wl, columns=['Wavelength', 'Efficiency'])
         data_csv = st.dataframe(data, height=380, width=200)
-    
-        # #save fiel as CSV
-        # data1 = data.to_csv(index=False)
-        # st.download_button(
-        #     label="Download data as CSV",
-        #     data=data1, 
-        #     file_name='data.csv',
-        #     mime='text/csv')
     
         #save fiel as txt
         data2 = data.to_csv(index=False, sep='\t')
@@ -186,56 +173,56 @@ if st.button("Run Spectral Absorption Prediction"):
             data=data2, 
             file_name='data.txt',
             mime='text/csv')
-    #------------------------------Hinh anh vat lieu---------------------------------------#
-    # def img3d(d):
-    #     D = d + 30 
-    #     h1 = D
-    #     mlab.options.offscreen = True
-    #     mlab.figure(size=(700, 700), bgcolor=(1, 1, 1))
-    #     x = np.array([0, 1, 1, 0, 0, 1, 1, 0])*(430*2+D+200) 
-    #     y = np.array([0, 0, 1, 1, 0, 0, 1, 1])*(430+D+200)
-    #     z = np.array([0, 0, 0, 0, -1, -1, -1, -1])*150
-    #     t = [[0, 1, 2], [2, 3, 0], 
-    #         [4, 5, 6], [6, 7, 4], 
-    #         [0, 1, 5], [5, 4, 0], 
-    #         [1, 2, 6], [1, 6, 5], 
-    #         [2, 3, 7], [7, 6, 2], 
-    #         [0, 3, 4], [4, 7, 3]]
-    #     mlab.triangular_mesh(x, y, z, t, color=(0.8, 0.7, 0.65))
-    #     #set
-    #     theta = np.linspace(0, 2*np.pi, 50)
-    #     h = np.linspace(0, h1, 50)
-    #     theta, h = np.meshgrid(theta, h)
+    ------------------------------Hinh anh vat lieu---------------------------------------#
+    def img3d(d):
+        D = d + 30 
+        h1 = D
+        mlab.options.offscreen = True
+        mlab.figure(size=(700, 700), bgcolor=(1, 1, 1))
+        x = np.array([0, 1, 1, 0, 0, 1, 1, 0])*(430*2+D+200) 
+        y = np.array([0, 0, 1, 1, 0, 0, 1, 1])*(430+D+200)
+        z = np.array([0, 0, 0, 0, -1, -1, -1, -1])*150
+        t = [[0, 1, 2], [2, 3, 0], 
+            [4, 5, 6], [6, 7, 4], 
+            [0, 1, 5], [5, 4, 0], 
+            [1, 2, 6], [1, 6, 5], 
+            [2, 3, 7], [7, 6, 2], 
+            [0, 3, 4], [4, 7, 3]]
+        mlab.triangular_mesh(x, y, z, t, color=(0.8, 0.7, 0.65))
+        #set
+        theta = np.linspace(0, 2*np.pi, 50)
+        h = np.linspace(0, h1, 50)
+        theta, h = np.meshgrid(theta, h)
     
-    #     for i in range(0, 3, 1):
-    #         for r in np.linspace(d/2, D/2, 30):
-    #             x3 = 430*i + D/2 + 100 + (r)*np.cos(theta)
-    #             y3 = D/2 + 100 + (r)*np.sin(theta)
-    #             mlab.mesh(x3, y3, h, color=(0.8, 0.7, 0.65))
+        for i in range(0, 3, 1):
+            for r in np.linspace(d/2, D/2, 30):
+                x3 = 430*i + D/2 + 100 + (r)*np.cos(theta)
+                y3 = D/2 + 100 + (r)*np.sin(theta)
+                mlab.mesh(x3, y3, h, color=(0.8, 0.7, 0.65))
     
-    #     for i in range(0, 3, 1):
-    #         for r in np.linspace(d/2, D/2, 30):
-    #             x3 = 430*i + D/2 + 100 + (r)*np.cos(theta)
-    #             y3 = 430 + D/2 + 100 + (r)*np.sin(theta)
-    #             mlab.mesh(x3, y3, h, color=(0.8, 0.7, 0.65))
-    # #--------------------------------------------------------------------#
-    # # # Chạy lệnh tạo máy chủ ảo Xvfb
-    # # os.system("Xvfb :1 -screen 0 1280x1024x24 -auth localhost")
-    # # # Thiết lập biến môi trường DISPLAY
-    # # os.environ["DISPLAY"] = ":1"
-    # with col1: 
-    #     st.subheader("Image of the material")
-    #     img3d(number)
-    #     def view(theta, phi):
-    #         mlab.view(azimuth=60, elevation=70, distance=2500)#hinh anh ban dau khi chua thay doi goc
-    #         mlab.view(theta, phi, distance=3000)
-    #         mlab.savefig("image.png")
-    #         image_path = 'image.png'
+        for i in range(0, 3, 1):
+            for r in np.linspace(d/2, D/2, 30):
+                x3 = 430*i + D/2 + 100 + (r)*np.cos(theta)
+                y3 = 430 + D/2 + 100 + (r)*np.sin(theta)
+                mlab.mesh(x3, y3, h, color=(0.8, 0.7, 0.65))
+    #--------------------------------------------------------------------#
+    # # Chạy lệnh tạo máy chủ ảo Xvfb
+    os.system("Xvfb :1 -screen 0 1280x1024x24 -auth localhost")
+    # Thiết lập biến môi trường DISPLAY
+    os.environ["DISPLAY"] = ":1"
+    with col1: 
+        st.subheader("Image of the material")
+        img3d(number)
+        def view(theta, phi):
+            mlab.view(azimuth=60, elevation=70, distance=2500)#hinh anh ban dau khi chua thay doi goc
+            mlab.view(theta, phi, distance=3000)
+            mlab.savefig("image.png")
+            image_path = 'image.png'
     
-    #         st.image(image_path)
-    #         # shutil.rmtree('3D')
-    #         os.remove(image_path)
+            st.image(image_path)
+            # shutil.rmtree('3D')
+            os.remove(image_path)
     
-    #     azimuth = st.slider('The azimuth argument specifies the angle "phi" on the x-y plane.', 0, 360, 60, 10)
-    #     elevation = st.slider('The elevation argument specifies the angle "theta" from the z axis.', 0, 180, 70, 10)
-    #     view(azimuth, elevation)
+        azimuth = st.slider('The azimuth argument specifies the angle "phi" on the x-y plane.', 0, 360, 60, 10)
+        elevation = st.slider('The elevation argument specifies the angle "theta" from the z axis.', 0, 180, 70, 10)
+        view(azimuth, elevation)
