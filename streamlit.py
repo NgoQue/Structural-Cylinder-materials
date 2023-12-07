51ed62bc76b142759dbe2b7a106d299ef7331df7
NgoQue <110570104+NgoQue@users.noreply.github.com>
2023-12-08 01:05:59 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -188,7 +188,6 @@ if st.button("Spectral Absorption Prediction"):
                 if (num_columns==3):
                     col0 = data_upload.iloc[1:, 1]
                     col1 = data_upload.iloc[1:, 2]
-                st.write(col0, col1)
                     
             if uploaded_file.type == "text/plain":  # Đối với file txt
                 data_upload = pd.read_csv(uploaded_file, delimiter='\t', header=None)
@@ -201,7 +200,6 @@ if st.button("Spectral Absorption Prediction"):
                 if (num_columns==3):
                     col0 = data_upload.iloc[1:, 1]
                     col1 = data_upload.iloc[1:, 2]
-                st.write(col0, col1)
                     
             if uploaded_file.type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":  # Đối với file Excel
                 data_upload = pd.read_excel(uploaded_file, engine="openpyxl")
@@ -214,7 +212,6 @@ if st.button("Spectral Absorption Prediction"):
                 if (num_columns==3):
                     col0 = data_upload.iloc[1:, 1]
                     col1 = data_upload.iloc[1:, 2]
-                st.write(col0, col1)
         #-----------------------------------------------------------------------------------#
             plt.figure(dpi = 300)
             fig, ax = plt.subplots()
