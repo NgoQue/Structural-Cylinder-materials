3a95b7d82464f285419dff010f929024c43b0935
NgoQue <110570104+NgoQue@users.noreply.github.com>
2023-12-07 23:00:27 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -93,8 +93,7 @@ def efficiency(abs_wl):
     H = H/2700
     return H
 #----------------Goi ham va ve do thi--------------------------------------------#
-st.write(" We calculate the spectral absorption of TiN-nanoring-based metamaterials 
-            by machine learning algorithms and data sets collected from CST simulations. The structure of the metamaterials is following"
+st.write(" We calculate the spectral absorption of TiN-nanoring-based metamaterials by machine learning algorithms and data sets collected from CST simulations. The structure of the metamaterials is following"
 st.image("3D/fig1.jpeg", use_column_width=True)
 if st.button("Spectral Absorption Prediction"):
     H = []
